da00515bb41e52575fa01596f9ef768c0f7844eb
lmgbsb <leandro.mesquita.galvao@gmail.com>
2021-09-13 22:26:57 -0300

    commit inicial do projeto

---
+++ src/main/java/lr/LivrariaRestApplication.java
@@ -0,0 +1,13 @@
+package lr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lr/LivrariaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package lr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
